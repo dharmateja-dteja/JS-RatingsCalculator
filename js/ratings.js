5fa3ef1cab34f6a346c2135271067b0933afee98
cloudcomputingworld <dharmateja.avasarala@gmail.com>
2023-01-15 14:17:17 +0530

    Completed the task

---
+++ js/ratings.js
@@ -0,0 +1,36 @@
+function collect_ratings()
+{
+    let ratings = {
+
+        count : 0,
+        sum :0,
+        average:0
+
+    }
+
+   let rating = 0;
+   const elements = document.querySelectorAll(".rating")
+
+   elements.forEach(element => {
+       let temp =  element.id.replace('star', '')
+        rating = parseInt(temp)
+        ratings.count += parseInt(element.value)
+        ratings.sum += parseInt(element.value) * rating;
+        console.log(ratings.sum)
+   });
+
+   if (ratings.count !== 0) 
+   {
+    ratings.average = ratings.sum/ratings.count
+   }
+     return ratings
+}
+
+
+
+document.addEventListener('change',() => {
+
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+
+})
